b3b0eacb28ce8d67d7d7270a14fe971ffd6dbb6c
Kei Ito <kei.itof@gmail.com>
2022-08-27 14:49:08 +0900

    feat: add HtmlParserContext

---
+++ src/HtmlParserContext.ts
@@ -0,0 +1,55 @@
+import {Error, JSON} from './es/global';
+import type {SerializeAttributeOptions} from './serializeAttributes';
+import type {HtmlAstNode} from './serializeHtmlAstNode';
+import {serializeHtmlAstNode} from './serializeHtmlAstNode';
+
+export class HtmlParserContext {
+
+    protected readonly stack: Array<HtmlAstNode> = [];
+
+    protected readonly root: Array<HtmlAstNode> = [];
+
+    protected get currentElement() {
+        return this.stack[0] as HtmlAstNode | undefined;
+    }
+
+    public *serialize(option: SerializeAttributeOptions): Generator<string> {
+        for (const node of this.root) {
+            yield* serializeHtmlAstNode(node, option);
+        }
+    }
+
+    protected enter(element: HtmlAstNode) {
+        const {currentElement} = this;
+        if (currentElement) {
+            currentElement.children.push(element);
+        } else {
+            this.root.push(element);
+        }
+        this.stack.unshift(element);
+    }
+
+    public onopentag(tag: string, attributes: Record<string, string>) {
+        this.enter({tag, attributes, children: []});
+    }
+
+    public ontext(text: string) {
+        const {currentElement} = this;
+        if (currentElement) {
+            currentElement.children.push(text);
+        } else if (text.trim()) {
+            throw new Error(`NoElementToAppend: ${JSON.stringify(text)}`);
+        }
+    }
+
+    public onclosetag(tag: string) {
+        const element = this.stack.shift();
+        if (!element) {
+            throw new Error(`UnexpectedClosing: ${tag}`);
+        }
+        if (tag !== element.tag) {
+            throw new Error(`UnmatchedTag: closing ${tag} but the context is ${element.tag}`);
+        }
+    }
+
+}
