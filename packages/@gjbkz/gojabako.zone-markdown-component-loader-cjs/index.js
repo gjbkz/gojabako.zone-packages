3c02961e2030042e8934cfe0772a34f59f593b8e
Kei Ito <kei.itof@gmail.com>
2022-08-28 15:20:49 +0900

    fix: use default import

--- packages/@gjbkz/gojabako.zone-markdown-component-loader-cjs/index.js
+++ packages/@gjbkz/gojabako.zone-markdown-component-loader-cjs/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 module.exports = async function markdownComponentLoader(source) {
-    const {loadMarkdownPage} = await import('@gjbkz/gojabako.zone-markdown-component-loader');
+    const {default: loadMarkdownPage} = await import('@gjbkz/gojabako.zone-markdown-component-loader');
     const code = await loadMarkdownPage.call(this, source);
     // console.info(code);
     return code;
--- packages/@gjbkz/gojabako.zone-markdown-page-loader-cjs/index.js
+++ packages/@gjbkz/gojabako.zone-markdown-page-loader-cjs/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 module.exports = async function markdownPageLoader(source) {
-    const {loadMarkdownPage} = await import('@gjbkz/gojabako.zone-markdown-page-loader');
+    const {default: loadMarkdownPage} = await import('@gjbkz/gojabako.zone-markdown-page-loader');
     const code = await loadMarkdownPage.call(this, source);
     // console.info(code);
     return code;
