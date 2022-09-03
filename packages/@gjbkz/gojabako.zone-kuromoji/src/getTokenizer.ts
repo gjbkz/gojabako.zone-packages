7bcbb220d9426ed5df03a1e36cc79c7e67d8c5b6
Kei Ito <kei.itof@gmail.com>
2022-09-04 00:27:11 +0900

    fix: use default export

--- packages/@gjbkz/gojabako.zone-kuromoji/src/getTokenizer.ts
+++ packages/@gjbkz/gojabako.zone-kuromoji/src/getTokenizer.ts
@@ -1,11 +1,12 @@
 import * as path from 'path';
 import type {IpadicFeatures, Tokenizer} from 'kuromoji';
-import {builder} from 'kuromoji';
+import kuromoji from 'kuromoji';
 
 export const getTokenizer = async (rootDirectory: string) => {
     const dicPath = path.join(rootDirectory, 'node_modules', 'kuromoji', 'dict');
     return await new Promise<Tokenizer<IpadicFeatures>>((resolve, reject) => {
-        builder({dicPath}).build((error: unknown, result) => {
+        // eslint-disable-next-line import/no-named-as-default-member
+        kuromoji.builder({dicPath}).build((error: unknown, result) => {
             if (error) {
                 reject(error);
             } else {
